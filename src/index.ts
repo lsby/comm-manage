b82a0fc4f7e85f71672297931b7ec962aedaec28
hbybyyang <2451759073@qq.com>
2021-09-12 15:40:45 +0800

    .

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,21 @@ import * as uuid from 'uuid'
 export interface 管理者 {
     发送: (data: string) => Promise<string>
     接收: (data: string) => Promise<void>
-    消费: (消息id: string, 超时时间?: number) => Promise<string>
+    消费: (消息id: string, 超时时间?: number) => Promise<string | null>
 }
 export interface 发送消息 {
     id: string
-    发送内容: string
+    data: string
 }
 export interface 返回消息 {
     id: string
-    返回内容: string
+    err: string | null
+    data: string | null
 }
 export interface 消息管理项 {
     id: string
     发送内容: string
-    返回内容: string
+    返回内容: string | null
     状态: '未回复' | '已回复'
 }
 
@@ -30,32 +31,28 @@ export default function 通信管理者(发送函数: (data: string) => Promise<
             var id = uuid.v4()
             var 消息: 发送消息 = {
                 id: id,
-                发送内容: data,
+                data: data,
             }
             await 发送函数(消息转字符串(消息))
             映射表[id] = {
-                ...消息,
-                返回内容: '',
+                id: id,
+                发送内容: data,
+                返回内容: null,
                 状态: '未回复',
             }
             return id
         },
         async 接收(data) {
             var 消息 = 字符串转消息(data) as 返回消息
-            if (
-                消息.id == null ||
-                typeof 消息.id != 'string' ||
-                消息.返回内容 == null ||
-                typeof 消息.返回内容 != 'string'
-            ) {
-                throw '消息解析错误:' + data
+            if (消息.id == null || typeof 消息.id != 'string') {
+                throw '必须有id:' + data
             }
             if (映射表[消息.id] == null) {
                 throw '这个id没有被发送:' + 消息.id
             }
 
             映射表[消息.id].状态 = '已回复'
-            映射表[消息.id].返回内容 = 消息.返回内容
+            映射表[消息.id].返回内容 = 消息.data
         },
         async 消费(id: string, 超时时间: number = 0) {
             return new Promise((res, rej) => {
