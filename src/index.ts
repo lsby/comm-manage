1d2dbeda40905f7de659b476bc02b91b6378579f
hbybyyang <2451759073@qq.com>
2021-09-12 15:21:38 +0800

    start

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import * as uuid from 'uuid'
+
+export interface 管理者 {
+    发送: (data: string) => Promise<string>
+    接收: (data: string) => Promise<void>
+    消费: (消息id: string, 超时时间?: number) => Promise<string>
+}
+export interface 发送消息 {
+    id: string
+    发送内容: string
+}
+export interface 返回消息 {
+    id: string
+    返回内容: string
+}
+export interface 消息管理项 {
+    id: string
+    发送内容: string
+    返回内容: string
+    状态: '未回复' | '已回复'
+}
+
+var 消息转字符串 = JSON.stringify
+var 字符串转消息 = JSON.parse
+
+export default function 通信管理者(发送函数: (data: string) => Promise<void>) {
+    var 映射表: { [消息id: string]: 消息管理项 } = {}
+    var r: 管理者 = {
+        async 发送(data) {
+            var id = uuid.v4()
+            var 消息: 发送消息 = {
+                id: id,
+                发送内容: data,
+            }
+            await 发送函数(消息转字符串(消息))
+            映射表[id] = {
+                ...消息,
+                返回内容: '',
+                状态: '未回复',
+            }
+            return id
+        },
+        async 接收(data) {
+            var 消息 = 字符串转消息(data) as 返回消息
+            if (
+                消息.id == null ||
+                typeof 消息.id != 'string' ||
+                消息.返回内容 == null ||
+                typeof 消息.返回内容 != 'string'
+            ) {
+                throw '消息解析错误:' + data
+            }
+            if (映射表[消息.id] == null) {
+                throw '这个id没有被发送:' + 消息.id
+            }
+
+            映射表[消息.id].状态 = '已回复'
+            映射表[消息.id].返回内容 = 消息.返回内容
+        },
+        async 消费(id: string, 超时时间: number = 0) {
+            return new Promise((res, rej) => {
+                if (映射表[id] == null) {
+                    rej('这个id没有被发送:' + id)
+                }
+
+                var 轮询时间 = 100
+                var 运行次数 = 0
+
+                function f() {
+                    运行次数++
+                    if (超时时间 != 0 && 运行次数 * 轮询时间 >= 超时时间) {
+                        return rej('超时:' + JSON.stringify({ id: 映射表[id].id, 发送内容: 映射表[id].发送内容 }))
+                    }
+                    if (映射表[id].状态 == '已回复') {
+                        return res(映射表[id].返回内容)
+                    }
+                    setTimeout(f, 100)
+                }
+                f()
+            })
+        },
+    }
+
+    return r
+}
---
+++ test/index.test.ts
@@ -0,0 +1,21 @@
+import lib from '../dist/'
+import * as tools from '@lsby/js_tools'
+
+var 通信管理者 = lib(async (data: string) => {})
+
+describe('基本测试', function () {
+    it('测试1', async function () {
+        var id = await 通信管理者.发送('这是消息')
+        await 通信管理者.接收(JSON.stringify({ id, 返回内容: '这是返回内容' }))
+        var x = await 通信管理者.消费(id)
+        tools.断言相等('这是返回内容', x)
+    })
+    it('测试2', async function () {
+        var id = await 通信管理者.发送('这是消息')
+        setTimeout(() => {
+            通信管理者.接收(JSON.stringify({ id, 返回内容: '这是返回内容' }))
+        }, 1000)
+        var x = await 通信管理者.消费(id)
+        tools.断言相等('这是返回内容', x)
+    })
+})
